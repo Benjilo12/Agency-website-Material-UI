10d744f1de9b516538617ee23bf7d39121168305
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2024-05-27 15:51:14 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CustomButton from "./components/Button/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="contained" color="slateBlue">
+        Test
+      </Button>
+      <Typography variant="body1">HELLO</Typography>
+      <CustomButton>Custom Props</CustomButton>
     </div>
   );
 }
